f82c9342ffdfd556b801911d84437d7d7edbfc53
Tim McDaniels <tim@weareconvoy.com>
2025-11-24 09:12:12 -0500

    Update 404.php

--- wp-content/themes/abivirginia/404.php
+++ wp-content/themes/abivirginia/404.php
@@ -1,4 +1,15 @@
 <?php get_header(); ?>
-	<h1>404 Not Found</h1>
-	<p>Oops! We can't seem to find the page you're looking for.<br>Error Code: 404</p>
+	<section class="section agent-sample-report-section is-secondary">
+        <div class="container">
+			<div class="has-text-centered mt-6">
+                <h2 class="title mb-4">404 Not Found</h2>
+				<p>Oops! We can't seem to find the page you're looking for.<br>Error Code: 404</p>
+				<div class="columns">
+					<div class="column buttons mt-4 has-text-centered pb-6">
+						<a class="button is-primary" href="/">Go to Home Page</a>
+					</div>
+				</div>
+            </div>
+        </div>
+    </section>
 <?php get_footer(); ?>
