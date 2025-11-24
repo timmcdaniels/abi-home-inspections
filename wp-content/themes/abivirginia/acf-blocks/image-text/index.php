e227d9a0880193d38806170d5284818880ce5cb8
Tim McDaniels <tim@weareconvoy.com>
2025-11-23 22:23:32 -0500

    add image ratio

--- wp-content/themes/abivirginia/acf-blocks/image-text/index.php
+++ wp-content/themes/abivirginia/acf-blocks/image-text/index.php
@@ -3,6 +3,8 @@
 	$image_id = get_field( 'image' );
 	$image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
 	list( $image ) = wp_get_attachment_image_src( $image_id, 'full' );
+	$image_ratio = get_field( 'image_ratio' );
+	if ( empty( $image_ratio ) ) $image_ratio = 'is-3by2';
 	$heading = get_field( 'heading' );
 	$text = get_field( 'text' );
 	$button_alignment = get_field( 'button_alignment' );
@@ -13,7 +15,7 @@
 			<div class="columns is-vcentered">
 				<?php if ( $image_location === 'Left' && ! empty( $image ) ): ?>
 				<div class="column is-6">
-					<figure class="<?php if ( get_field( 'use_blue_background' ) ): ?>blue-bg<?php else: ?>image is-4by3<?php endif; ?>">
+					<figure class="<?php if ( get_field( 'use_blue_background' ) ): ?>blue-bg<?php else: ?>image <?php echo $image_ratio; ?><?php endif; ?>">
 						<img src="<?php echo $image; ?>" alt="<?php echo $image_alt; ?>">
 					</figure>
 				</div>
@@ -25,7 +27,7 @@
 				</div>
 				<?php if ( $image_location === 'Right' && ! empty( $image ) ): ?>
 				<div class="column is-6">
-					<figure class="<?php if ( get_field( 'use_blue_background' ) ): ?>blue-bg<?php else: ?>image is-4by3<?php endif; ?>">
+					<figure class="<?php if ( get_field( 'use_blue_background' ) ): ?>blue-bg<?php else: ?>image <?php echo $image_ratio; ?><?php endif; ?>">
 						<img src="<?php echo $image; ?>" alt="<?php echo $image_alt; ?>">
 					</figure>
 				</div>
